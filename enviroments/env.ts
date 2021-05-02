68efe9afa6dad0fd2202403c5501d0d9aa91a0e6
luis <luiscg404@gmail.com>
2021-05-02 18:24:22 -0500

    Se actualiza ip

--- enviroments/env.ts
+++ enviroments/env.ts
@@ -3,7 +3,7 @@ export default {
         PORT:3000
     },
     MONGODB:{
-        HOST:'localhost',
+        HOST:'192.168.1.72',
         PORT:'27017',
         USER_NAME:'',
         USER_PASSWORD:'',
