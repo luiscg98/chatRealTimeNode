2eff667c3dc8735a37f57375b1b02fa0ee5ae144
luis <luiscg404@gmail.com>
2021-05-02 17:07:22 -0500

    primer commit

---
+++ dist/enviroments/env.js
@@ -0,0 +1,17 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = {
+    API: {
+        PORT: 3000
+    },
+    MONGODB: {
+        HOST: 'localhost',
+        PORT: '27017',
+        USER_NAME: '',
+        USER_PASSWORD: '',
+        DATABASE: 'dbMTWyM'
+    },
+    TOKEN: {
+        EXPIRES: 60 * 60 * 4
+    }
+};
